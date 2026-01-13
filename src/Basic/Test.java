bac9c5bb779cc2c753753e7550a123ce7d717b70
Tamalika <tam29barsha@gmail.com>
2026-01-13 19:18:58 +0530

    this is commit from chandannagar

--- src/Basic/Test.java
+++ src/Basic/Test.java
@@ -7,20 +7,6 @@ public class Test {
 	
 	public static void main(String [] args) {
 		
-//		byte []ch= {100,101,102,103,104};
-//		String s=new String(ch);
-//		System.out.println(s.charAt(2));
-//		System.out.println(s.concat(s));
-//		System.out.println(s.substring(2));
-//		System.out.println(s.substring(2, 5));
-//		System.out.println(s.replace('e', '@'));
-//		System.out.println(s.toLowerCase() +" "+s.toUpperCase());
-		
-//		List<String> l=new ArrayList<String>();
-//		l.add("Hira");
-//		l.add("Sita");
-//		System.out.println(l.toString());
-		
 		String s="silent1234";
 		int count=0;
 		char c[]=s.toCharArray();
