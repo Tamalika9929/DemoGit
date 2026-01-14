45ceca3324008689ef035312305122a34dc00bb6
Tamalika <tam29barsha@gmail.com>
2026-01-14 20:38:07 +0530

    committed in develop branch at 20.37 pm

---
+++ src/Basic/NewTest.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Hello",temp="";
+		
+		//char c[]=s.toCharArray();
+		
+		for(int i=s.length()-1;i>=0;i--) {
+			
+			temp=temp+s.charAt(i);
+		}
+		System.out.print(temp);
+
+	}
+
+}
